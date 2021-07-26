2de626e19ad97263dce81d178723bb33a8762122
mwruszczyk <marcin.ruszczyk@gazeta.pl>
2021-07-26 21:47:15 +0200

    Compare numbers

---
+++ BrainTeasersTraining/CompareValues/CompareRun.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BrainTeasersTraining.CompareValues
+{
+
+    //Write a C# Sharp program to compute the sum of the two given integer values.
+    //If the two values are the same, then return triple their sum
+    class CompareRun
+    {
+
+        public void CompareNumber()
+        {
+    
+            int number1 = 0;
+            int number2 = 0;
+            string num = "";
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Give me first proper number.");
+                num = Console.ReadLine();
+
+            } while (!Int32.TryParse(num, out number1));
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Give me second proper number.");
+                num = Console.ReadLine();
+
+            } while (!Int32.TryParse(num, out number2));
+
+            Console.Write("The output is:");
+            Console.WriteLine(number1 == number2 ? (number1 + number2) * 3 : (number1 + number2));
+            Console.ReadKey();
+       }
+        
+        
+    }
+}
